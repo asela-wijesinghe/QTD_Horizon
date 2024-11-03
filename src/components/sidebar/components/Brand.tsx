06756e1f8cbdc0b4a0d15aedc780c5c091e02703
Asela Wijesinghe <asela@redem.io>
2024-11-04 00:08:28 +0100

    add logo PNG

--- src/components/sidebar/components/Brand.tsx
+++ src/components/sidebar/components/Brand.tsx
@@ -1,17 +1,16 @@
 'use client';
 // Chakra imports
-import { Flex, useColorModeValue } from '@chakra-ui/react';
+import { Flex, Img, useColorModeValue } from '@chakra-ui/react';
 
 import { HorizonLogo } from '@/components/icons/Icons';
 import { HSeparator } from '@/components/separator/Separator';
-
 export function SidebarBrand() {
   //   Chakra color mode
   let logoColor = useColorModeValue('navy.700', 'white');
 
   return (
     <Flex alignItems="center" flexDirection="column">
-      <HorizonLogo h="26px" w="146px" my="30px" color={logoColor} />
+      <Img src="/img/layout/logo.png" alt="Logo" style={{  padding: '30px 40px' }} />
       <HSeparator mb="20px" w="284px" />
     </Flex>
   );
